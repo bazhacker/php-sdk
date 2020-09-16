595a9c0b95ecfbf3d09634675a82a13a223a65fd
Macbook Pro 15 Touchbar <bazhacker@gmail.com>
2020-09-16 10:49:06 -0500

    Update configApp.php

--- configApp.php
+++ configApp.php
@@ -3,27 +3,27 @@
 /* Go to My Apps dashboard: https://developers.mercadolibre.com.ar/apps/home, and get the information you need in order to the following enviroment variables */
 
 /* Your Application Id */
-$appId = getenv('App_ID');
+// $appId = getenv('5011524864778604');
 
 /* Your Secret Key */
-$secretKey = getenv('Secret_Key');
+// $secretKey = getenv('RFbYQHHKxaWLeh4cx2y7kLFqXR8QPvIg');
 
 /* The Redirect url */
-$redirectURI = getenv('Redirect_URI');
+// $redirectURI = getenv('https://growyoursocials.com/mlpublica/php-sdk-master');
 
 /* The site id of the country where your application will work.
 If you don't know your site_id go to our sites resources: https://api.mercadolibre.com/sites  */
-$siteId = 'MLA';
+// $siteId = 'MLM';
 
 
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////
 //If you don't use Heroku use the next config
 
-// $appId = 'App_ID';
+ $appId = '5011524864778604';
 
-// $secretKey = 'Secret_Key';
+ $secretKey = 'RFbYQHHKxaWLeh4cx2y7kLFqXR8QPvIg';
 
-// $redirectURI = 'Redirect_URI';
+ $redirectURI = 'https://growyoursocials.com/mlpublica/php-sdk-master';
 
-// $siteId = 'MLB';
+ $siteId = 'MLM';
